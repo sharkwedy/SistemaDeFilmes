f2a354c51170c7261795dce7295271f5c93d1527
sharkwedy <talls.urin@hotmail.com>
2018-09-05 05:02:36 -0300

    Thales

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+
+const { mongoose } = require('./database');
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors({origin: 'http://localhost:4200'}));
+
+// Routes
+app.use('/api/movies', require('./routes/movie.routes'));
+
+// Starting the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
